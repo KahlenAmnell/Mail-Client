3e757435b04293f75b7271839e368b74339f8857
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-06-27 12:54:03 +0200

    Configure Slider in Options

--- src/main/java/pl/bernat/controller/OptionWindowController.java
+++ src/main/java/pl/bernat/controller/OptionWindowController.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
+import javafx.util.StringConverter;
 import pl.bernat.EmailManager;
 import pl.bernat.view.ColorTheme;
+import pl.bernat.view.FontSize;
 import pl.bernat.view.ViewFactory;
 
 import java.net.URL;
@@ -37,6 +39,34 @@ public class OptionWindowController extends BaseController implements Initializa
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setUpThemePicker();
+        setUpSizePicker();
+    }
+
+    private void setUpSizePicker() {
+        fontSizePicker.setMin(0);
+        fontSizePicker.setMax(FontSize.values().length-1);
+        fontSizePicker.setValue(viewFactory.getFontSize().ordinal());
+        fontSizePicker.setMajorTickUnit(1);
+        fontSizePicker.setMinorTickCount(0);
+        fontSizePicker.setBlockIncrement(1);
+        fontSizePicker.setSnapToTicks(true);
+        fontSizePicker.setShowTickMarks(true);
+        fontSizePicker.setShowTickLabels(true);
+        fontSizePicker.setLabelFormatter(new StringConverter<Double>() {
+            @Override
+            public String toString(Double aDouble) {
+                int i = aDouble.intValue();
+                return FontSize.values()[i].toString();
+            }
+
+            @Override
+            public Double fromString(String s) {
+                return null;
+            }
+        });
+        fontSizePicker.valueProperty().addListener((obs, oldVal, newVal) -> {
+            fontSizePicker.setValue(newVal.intValue());
+        });
     }
 
     private void setUpThemePicker() {
