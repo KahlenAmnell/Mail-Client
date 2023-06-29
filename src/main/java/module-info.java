0e0f147eb74072558e1746e6c00b3486f9d332af
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-06-29 09:56:00 +0200

    Add libraries to manage email

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module Mail.Client {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens pl.bernat;
     opens pl.bernat.controller;
