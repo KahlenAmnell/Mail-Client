ddce1c7be7a22a32c132204391d46e51d36ded18
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-07-10 22:24:33 +0200

    Update folders with new messages

--- src/main/java/pl/bernat/EmailManager.java
+++ src/main/java/pl/bernat/EmailManager.java
@@ -2,20 +2,34 @@ package pl.bernat;
 
 import javafx.scene.control.TreeItem;
 import pl.bernat.controller.services.FetchFoldersService;
+import pl.bernat.controller.services.FolderUpdaterService;
 import pl.bernat.model.EmailAccount;
 import pl.bernat.model.EmailTreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+    private FolderUpdaterService folderUpdaterService;
     //Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
 
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList(){
+        return this.folderList;
+    }
 
+    public EmailManager(){
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
