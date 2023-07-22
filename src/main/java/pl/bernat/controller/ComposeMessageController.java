d29bc1278a2400054bcec43c915235a229b24d15
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-07-22 19:47:21 +0200

    Attach attachments in sending mail

--- src/main/java/pl/bernat/controller/ComposeMessageController.java
+++ src/main/java/pl/bernat/controller/ComposeMessageController.java
@@ -2,22 +2,31 @@ package pl.bernat.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import pl.bernat.EmailManager;
 import pl.bernat.controller.services.EmailSenderService;
 import pl.bernat.model.EmailAccount;
 import pl.bernat.view.ViewFactory;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ComposeMessageController extends BaseController implements Initializable {
-
+    @FXML
+    private HBox attachmentsHBox;
+    private List<File> attachments = new ArrayList<File>();
     @FXML
     private Label errorLabel;
 
@@ -36,14 +45,14 @@ public class ComposeMessageController extends BaseController implements Initiali
     @FXML
     private ChoiceBox<EmailAccount> emailAccountChoice;
 
-
     @FXML
     void sendButtonAction() {
         EmailSenderService emailSenderService = new EmailSenderService(
             emailAccountChoice.getValue(),
                 subjectTextField.getText(),
                 recipientTextField.getText(),
-                htmlEditor.getHtmlText()
+                htmlEditor.getHtmlText(),
+                attachments
         );
         emailSenderService.start();
         emailSenderService.setOnSucceeded(e -> {
@@ -62,6 +71,16 @@ public class ComposeMessageController extends BaseController implements Initiali
             }
         });
     }
+
+    @FXML
+    void addAttach() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if(selectedFile != null ){
+            attachments.add(selectedFile);
+            attachmentsHBox.getChildren().add(setLabel(selectedFile.getName()));
+        }
+    }
     public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
     }
@@ -70,5 +89,11 @@ public class ComposeMessageController extends BaseController implements Initiali
     public void initialize(URL url, ResourceBundle resourceBundle) {
         emailAccountChoice.setItems(emailManager.getEmailAccounts());
         emailAccountChoice.setValue(emailManager.getEmailAccounts().get(0));
+    }
+    private Label setLabel(String fileName){
+        Label label = new Label(fileName);
+        label.setPadding(new Insets(4));
+        label.setStyle("-fx-background-color: White");
+        return label;
     }
 }
